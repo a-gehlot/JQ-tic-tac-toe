762e82754c060b9996cc76ab54c15ff8a9af0613
a-gehlot <andrew.gehlot@gmail.com>
2021-11-19 22:08:55 -0500

    Added data position to each li item

--- src/ttt-view.js
+++ src/ttt-view.js
@@ -5,16 +5,29 @@ class View {
     this.setupBoard();
   }
 
-  bindEvents() {}
+  bindEvents() {
+    let self = this;
+    let $li = $("li");
+    $li.click(function() {
+      self.makeMove($(this))
+    })
+  }
 
-  makeMove($square) {}
+  makeMove($square) {
+    console.log("move made")
+  }
 
   setupBoard() {
     this.$el.append("<ul>");
     let $ul = this.$el.find("ul");
-    for (let i = 0; i < 9; i++) {
-        $ul.append("<li>");
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        $ul.append("<li>")
+        let $appended_item = $("ul li").last();
+        $appended_item.attr("data-pos", [i, j]);
+      }
     }
+    this.bindEvents();
   }
 
 }
